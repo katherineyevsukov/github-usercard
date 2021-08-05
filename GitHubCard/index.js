638cc6e4f96317e75e3fa369971f16066755a3e0
Kat <kyevsukov@gmail.com>
2021-08-05 19:03:29 -0400

    finished MVP

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -4,16 +4,17 @@
     https://api.github.com/users/<your name>
 */
 
-axios.get(' https://api.github.com/users/katherineyevsukov')
-  .then(res => {
-    console.log(res)
-   const kat = cardCreator(res.data)
-   document.querySelector('.cards').appendChild(kat)
-   console.log(kat)
+axios
+  .get(" https://api.github.com/users/katherineyevsukov")
+  .then((res) => {
+    console.log(res);
+    const kat = cardCreator(res.data);
+    document.querySelector(".cards").appendChild(kat);
+    console.log(kat);
   })
-  .catch(err => {
-    console.error(err)
-  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -39,8 +40,30 @@ axios.get(' https://api.github.com/users/katherineyevsukov')
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [
+  "tetondan",
+  "dustinmyers",
+  "justsml",
+  "luishrd",
+  "bigknell",
+];
 
+function addUsers(array) {
+  array.forEach((user) => {
+    axios
+      .get(`https://api.github.com/users/${user}`)
+      .then((res) => {
+        console.log(res.data);
+        const userCard = cardCreator(res.data);
+        document.querySelector(".cards").appendChild(userCard);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+}
+
+addUsers(followersArray);
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
@@ -61,53 +84,62 @@ const followersArray = [];
     </div>
 */
 
-function cardCreator({ avatar_url, name, login, location, html_url, followers, following, bio }){
-  const card = document.createElement('div')
-  const pic = document.createElement('img')
-  const info = document.createElement('div')
-  const myName = document.createElement('h3')
-  const myUsername = document.createElement('p')
-  const place = document.createElement('p')
-  const profile = document.createElement('p')
-  const link = document.createElement('a')
-  const myFollowers = document.createElement('p')
-  const myFollowing = document.createElement('p')
-  const about = document.createElement('p')
+function cardCreator({
+  avatar_url,
+  name,
+  login,
+  location,
+  html_url,
+  followers,
+  following,
+  bio,
+}) {
+  const card = document.createElement("div");
+  const pic = document.createElement("img");
+  const info = document.createElement("div");
+  const myName = document.createElement("h3");
+  const myUsername = document.createElement("p");
+  const place = document.createElement("p");
+  const profile = document.createElement("p");
+  const link = document.createElement("a");
+  const myFollowers = document.createElement("p");
+  const myFollowing = document.createElement("p");
+  const about = document.createElement("p");
 
+  profile.innerHTML = "Profile: ";
 
-  card.appendChild(pic)
-  card.appendChild(info)
-  info.appendChild(myName)
-  info.appendChild(myUsername)
-  info.appendChild(place)
-  info.appendChild(profile)
-  profile.appendChild(link)
-  info.appendChild(myFollowers)
-  info.appendChild(myFollowing)
-  info.appendChild(about)
-  console.log(profile)
+  card.appendChild(pic);
+  card.appendChild(info);
+  info.appendChild(myName);
+  info.appendChild(myUsername);
+  info.appendChild(place);
+  info.appendChild(profile);
+  profile.appendChild(link);
+  info.appendChild(myFollowers);
+  info.appendChild(myFollowing);
+  info.appendChild(about);
+  console.log(profile);
 
-  card.classList.add('card')
-  pic.setAttribute('src', `${avatar_url}`)
-  pic.setAttribute('alt', `This is a picture of ${name}`)
-  info.classList.add('card-info')
-  myName.classList.add('name')
-  myName.textContent = `Name: ${name}`
-  myUsername.classList.add('username')
-  myUsername.textContent = `Username: ${login}`
-  place.textContent = `Location: ${location ? location : 'I have no location'}`
-  profile.textContent = 'Profile:'
-  link.setAttribute('src', `${html_url}`)
-  link.textContent = html_url
-  myFollowers.textContent = `Followers: ${followers}`
-  myFollowing.textContent = `Following: ${following}`
-  about.textContent = `Bio: ${bio ? bio : 'I have no bio'}`
-  
-  console.log(profile)
-  console.log(link)
-  
+  card.classList.add("card");
+  pic.setAttribute("src", avatar_url);
 
-  return card
+  pic.setAttribute("alt", `This is a picture of ${name}`);
+  info.classList.add("card-info");
+  myName.classList.add("name");
+  myName.textContent = `Name: ${name}`;
+  myUsername.classList.add("username");
+  myUsername.textContent = `Username: ${login}`;
+  place.textContent = `Location: ${location ? location : "I have no location"}`;
+  link.setAttribute("href", `${html_url}`);
+  link.textContent = html_url;
+  myFollowers.textContent = `Followers: ${followers}`;
+  myFollowing.textContent = `Following: ${following}`;
+  about.textContent = `Bio: ${bio ? bio : "I have no bio"}`;
+
+  console.log(profile);
+  console.log(link);
+
+  return card;
 }
 
 /*
